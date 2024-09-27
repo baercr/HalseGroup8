543639ff690a4dd91cc14a39fdfe7e57f270825b
baercr <baercr@mail.uc.edu>
2024-09-27 14:15:49 -0400

    Main Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnButtonStart() 
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnButtonQuit() 
+    { 
+        Application.Quit();
+    }
+
+    public void OnButtonMainMenu()
+    { 
+        SceneManager.LoadScene(0); 
+    }
+}

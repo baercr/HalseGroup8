93d12f735affec56c8719256ab7cb5def78b229b
baercr <baercr@mail.uc.edu>
2024-10-10 15:29:47 -0400

    Shift Select Script

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -7,16 +7,21 @@ public class Menu : MonoBehaviour
 {
     public void OnButtonStart() 
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1);      // Loads Shift Selection
     }
 
     public void OnButtonQuit() 
     { 
-        Application.Quit ();
+        Application.Quit ();            // Quits Game, will not quit if running from editor
     }
 
     public void OnButtonMainMenu()
     { 
-        SceneManager.LoadScene(0); 
+        SceneManager.LoadScene(0);      // Loads Menu Screen
+    }
+
+    public void OnShiftSelect() 
+    { 
+        SceneManager.LoadScene (2);     // Loads Game Scene
     }
 }

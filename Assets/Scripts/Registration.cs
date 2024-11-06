af1bc32a8fbdc532a922dc7cf5ce8647e7a7cb6f
baercr <baercr@mail.uc.edu>
2024-11-06 14:37:05 -0500

    Update Registration.cs

--- Assets/Scripts/Registration.cs
+++ Assets/Scripts/Registration.cs
@@ -6,39 +6,39 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Networking;
 
-public class Registration : MonoBehaviour
+public class Web : MonoBehaviour
 {
     public TMP_InputField nameField;
     public TMP_InputField passwordField;
 
     public Button btnRegister;
 
-    public void CallRegister()
-    {
+    void Start() {
         StartCoroutine(Register());
     }
 
-    IEnumerator Register()
-    {
-        List<IMultipartFormSection> form = new List<IMultipartFormSection>();
-        form.Add(new MultipartFormDataSection("name", nameField.text));
-        form.Add(new MultipartFormDataSection("password", passwordField.text));
+    IEnumerator Register() {
+        //List<IMultipartFormSection> form = new List<IMultipartFormSection>();
+            //form.Add(new MultipartFormDataSection("name", nameField.text));
+            //form.Add(new MultipartFormDataSection("password", passwordField.text));
 
-        UnityWebRequest myWr = UnityWebRequest.Post("http://localhost/sqlconnect/register.php", form);
-        yield return myWr.SendWebRequest();
-        if (myWr.result == UnityWebRequest.Result.ConnectionError)
-        {
+        UnityWebRequest myWr = UnityWebRequest.Get("http://localhost/UnityDB/GetUsers.php");
+            yield return myWr.SendWebRequest();
+
+        if (myWr.result == UnityWebRequest.Result.ConnectionError) {
             Debug.Log(myWr.error);
             UnityEngine.SceneManagement.SceneManager.LoadScene(0);
         }
-        else
-        {
-             Debug.Log(myWr.downloadHandler.text);
+        else {
+            // Show results as text
+            Debug.Log(myWr.downloadHandler.text);
+
+            // Show results as binary data
+            byte[] results = myWr.downloadHandler.data;
         }
     }
 
-    public void VerifyInputs()
-    {
+    public void VerifyInputs() {
         btnRegister.interactable = (nameField.text.Length >= 3 && passwordField.text.Length >= 5);
     }
 }

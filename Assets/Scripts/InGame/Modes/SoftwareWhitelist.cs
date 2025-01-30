2e3835b709fdede05fff691348bc3c8ae8bdc26d
RobBro24 <79070744+RobBro24@users.noreply.github.com>
2025-01-29 19:10:59 -0500

    Script added to change program in array to false

---
+++ Assets/Scripts/InGame/Modes/SoftwareWhitelist.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoftwareWhitelist : MonoBehaviour
+{
+    public Texture2D[] images; // Assign images in the Unity Inspector
+    private bool[,] appArray = new bool[5, 5];
+    private Texture2D[,] imageArray = new Texture2D[5, 5];
+
+    void Start()
+    {
+        // Initialize appArray with true (approved/whitelisted)
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                appArray[i, j] = true;
+                // Assign images to imageArray
+                imageArray[i, j] = images[Random.Range(0, images.Length)];
+            }
+        }
+
+        // Run random number generators
+        int row = Random.Range(0, 5);
+        int column = Random.Range(0, 5);
+
+        Debug.Log("Target app at row: " + row + " column: " + column);
+
+        // Change the selected app to false (not whitelisted)
+        appArray[row, column] = false;
+
+        // Print the array status to the console
+        PrintArray();
+    }
+
+    void PrintArray()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            string row = "";
+            for (int j = 0; j < 5; j++)
+            {
+                row += appArray[i, j] ? "1 " : "0 ";
+            }
+            Debug.Log(row);
+        }
+    }
+}
+

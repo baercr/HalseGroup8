0e86b7b2197f34545fdc69bb225038da8aa49152
jwasilko25 <105300237+jwasilko25@users.noreply.github.com>
2025-02-09 15:56:14 -0500

    added in faulty usernames 40% chance

--- Assets/Scripts/InGame/Modes/NameGenerator.cs
+++ Assets/Scripts/InGame/Modes/NameGenerator.cs
@@ -10,12 +10,19 @@ public class NameGenerator : MonoBehaviour
     private string[] firstNames;
     private string[] lastNames;
     public TMP_Text nameText;
+    public TMP_Text usernameText;
+
+    private string generatedFirstName;
+    private string generatedLastName;
 
     void Start()
     {
         ReadNamesFromFile();
         string randomFullName = GenerateRandomFullName();
         nameText.text = "Full Name: " + randomFullName;
+
+        string randomUsername = GenerateRandomUsername();
+        usernameText.text = "Username: " + randomUsername;
     }
 
     void ReadNamesFromFile()
@@ -36,8 +43,42 @@ public class NameGenerator : MonoBehaviour
     {
         int firstNameIndex = Random.Range(0, firstNames.Length);
         int lastNameIndex = Random.Range(0, lastNames.Length);
-        return firstNames[firstNameIndex] + " " + lastNames[lastNameIndex];
+        generatedFirstName = firstNames[firstNameIndex];
+        generatedLastName = lastNames[lastNameIndex];
+        return generatedFirstName + " " + generatedLastName;
     }
+
+    string GenerateRandomUsername()
+    {
+        string digits = "0123456789";
+        string firstLetter = generatedFirstName.Substring(0, 1).ToUpper();
+        string lastLetter = generatedLastName.Substring(0, 1).ToUpper();
+
+        string randomDigits = "";
+        randomDigits += digits[Random.Range(0, digits.Length)];
+        randomDigits += digits[Random.Range(0, digits.Length)];
+
+        string randomUsername = firstLetter + lastLetter + randomDigits;
+
+        bool isFaulty = Random.Range(0f, 1f) < 0.4f;
+        if (isFaulty)
+        {
+            int faultyType = Random.Range(0, 2);
+            switch (faultyType)
+            {
+                case 0:
+                    randomUsername = firstLetter + firstLetter[Random.Range(0, firstLetter.Length)] + lastLetter + randomDigits;
+                    break;
+                case 1:
+                    randomUsername = firstLetter + lastLetter + randomDigits + digits[Random.Range(0, digits.Length)];
+                    break;
+            }
+        }
+
+        return randomUsername;
+
+    }
+
 
 
     // Update is called once per frame

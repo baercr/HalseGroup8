f736f2b2f066f540a6361ec40e5413d9666479fa
baercr <baercr@mail.uc.edu>
2025-03-01 14:32:57 -0500

    Terminal Phase 4

--- Assets/Scenes/Terminal/Interpreter.cs
+++ Assets/Scenes/Terminal/Interpreter.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 
 public class Interpreter : MonoBehaviour
 {
+    Dictionary<string, string> colors = new Dictionary<string, string>()
+    {
+        {"black", "#021b21" },
+        {"gray", "#555d71" },
+        {"red", "#ff5879" },
+        {"yellow", "#f2f1b9" },
+        {"blue", "#9ed9d8" },
+        {"purple", "#d926ff" },
+        {"orange", "#ef5847" }
+    };
+
     List<string> response = new List<string>();
 
     public List<string> Interpret(string userInput)
@@ -16,10 +27,9 @@ public class Interpreter : MonoBehaviour
 
         if (args[0] == "help")
         {
-            response.Add("If you want to use the terminal, type \"boop\" ");
-            response.Add("This is the second line that we are returning");
-
-            return response;
+            ListEntry("help", "returns a list of commands");
+            ListEntry("stop", "pauses the game");
+            ListEntry("run", "resumes the game");
         }
         if (args[0] == "boop")
         {
@@ -33,5 +43,18 @@ public class Interpreter : MonoBehaviour
 
             return response;
         }
+    }
+
+    public string ColorString(string s, string color)
+    {
+        string leftTag = "<color=" + color + ">";
+        string rightTag = "</color=" + color + ">";
+
+        return leftTag + s + rightTag;
+    }
+
+    void ListEntry(string a,  string b)
+    {
+        response.Add(ColorString(a, colors["orange"]) + ": " + ColorString(b, colors["yellow"]));
     }
 }
